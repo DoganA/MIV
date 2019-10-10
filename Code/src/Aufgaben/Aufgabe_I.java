c9794bc7a28d4b412f7f61d46e74406ffdbb1f9b
Dogan <d.alk@hotmail.de>
2019-10-10 10:42:12 +0200

    init

---
+++ Code/src/Aufgaben/Aufgabe_I.java
@@ -0,0 +1,10 @@
+package Aufgaben;
+
+import javax.swing.JFrame;
+
+public class Aufgabe_I extends JFrame {
+	
+	
+	
+
+}
---
+++ Code/src/Aufgaben/Oberfläche.java
@@ -0,0 +1,45 @@
+package Aufgaben;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Oberfläche extends JFrame {
+
+	private JPanel contentPane;
+	private JButton startButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Oberfläche frame = new Oberfläche();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Oberfläche() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		setContentPane(startButton);
+	}
+
+}
