10376dc7b9765c870cddf3e8f458936703996951
Dogan <d.alk@hotmail.de>
2019-12-22 22:17:33 +0100

    Uebung3

--- Übung3/Quellcode/src/Main.java
+++ Übung3/Quellcode/src/Main.java
@@ -1,31 +1,52 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.io.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.io.File;
-
 import javax.imageio.ImageIO;
 
-public class Main {
-	BufferedImage bufImg;
-	Graphics gBuf;
-	
-	public static void main(String[] args) {
-		
-	}
-	/**
-	 * 
-	 */
-	private void createPicture() {
-		try {
-			Color col = new Color();
-			bufImg = new BufferedImage(960, 560,BufferedImage.TYPE_INT_RGB);
-			gBuf = bufImg.createGraphics();
-			gBuf.setColor(col.BLUE);
-			ImageIO.write(gBuf, "bmp", new File("C://Users/Rou/Desktop/image.bmp"));
-		} catch (IOException e) {
-			System.out.println("Exception occured :" + e.getMessage());
-		}
-	}
 
+class BitmapExample {
+
+  public static void main(String args[]) {
+    BufferedImage bufImg = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
+    Graphics gBuf = bufImg.createGraphics();
+
+    try {
+    //Rot
+    gBuf.setColor(new Color(255, 0, 0));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("Rot.bmp"));
+
+
+    //grün
+    gBuf.setColor(new Color(0, 255, 0));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("grün.bmp"));
+    
+    //Blau
+    gBuf.setColor(new Color(0, 255, 255));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("blau.bmp"));
+
+    
+    //Rot-Grün
+    gBuf.setColor(new Color(255, 255, 0));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("rot_grün.bmp"));
+   
+    //Rot-Blau
+    gBuf.setColor(new Color(0, 255, 255));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("rot_blau.bmp"));
+
+
+    //grün-Blau
+    gBuf.setColor(new Color(0, 255, 255));
+    gBuf.fillRect(0, 0, 960, 540);
+    ImageIO.write(bufImg, "bmp", new File("gruen_blau.bmp"));
+
+    }
+    catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
 }
